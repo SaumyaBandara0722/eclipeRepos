82f20155338f80feef35371ae7738ff2e37d5e02
SaumyaBandara0722 <saumya19970722@gmail.com>
2020-02-18 23:59:10 -0800

    second commit

--- src/com/sliit/main/Main.java
+++ src/com/sliit/main/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("This is my first project at SLIIT");    
+		System.out.println("This is my first project at SLIIT 1111");    
 	}
 
 }
